664dd64c463c2ba2dad32a84d1bf30f63ece0bfd
Nugroho Adi Pramono <nugnux@gmail.com>
2024-09-18 10:06:55 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -3,6 +3,8 @@ import streamlit as st
 st.title('FISIKA KOMPUTASI AWAN')
 st.write('Situs Official Puan')
 st.latex(r''' \int_a^b \frac{\partial^2 y}{\partial x^2}dx''')
+st.page_link("http://www.google.com", label="Google", icon="🌎")
+st.page_link("http://www.nugroho.xyz", label="Nug", icon="🌎")
 
 print('Halo')
 print('Tadaa...')
