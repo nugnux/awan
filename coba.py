2b05cf23598a68b8917b7a37f850498112e19137
Nugroho Adi Pramono <nugnux@gmail.com>
2024-09-18 10:04:51 +0700

    Update coba.py

--- coba.py
+++ coba.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.title('FISIKA KOMPUTASI AWAN')
 st.write('Situs Official Puan')
+st.latex(r''' \int_a^b \frac{\partial^2 y}{\partial x^2}dx''')
 
 print('Halo')
 print('Tadaa...')
